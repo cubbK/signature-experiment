f9bda94755f0b3d5e5e0867ad58745c795fb6adb
Dan <danpopa1998@gmail.com>
2019-02-05 22:35:31 +0200

    make everything work

--- app/index.js
+++ app/index.js
@@ -1,5 +1,23 @@
 const argv = require("minimist")(process.argv.slice(2));
 const pc = process.env.PC;
 
-console.log(argv);
-console.log(pc);
+const axios = require("axios");
+
+console.log("PC: " + pc)
+
+async function main() {
+  try {
+    const response = await axios.post(
+      "http://validation-server:4000/activate",
+      {
+        license: "key1",
+        pc: pc
+      }
+    );
+    console.log(response.data);
+  } catch (err) {
+    console.log(err.response.data);
+  }
+}
+
+main();
